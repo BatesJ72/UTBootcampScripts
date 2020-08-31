e74181e729866c8d4fb27b4011f441bc5ecb1e30
Jessica Bates <bates.jessica72@gmail.com>
2020-08-31 13:08:42 +0000

    Updates

---
+++ Python_Scripts/pandas_notes.py
@@ -0,0 +1,57 @@
+import pandas 
+
+row_oriented_data = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [6, 1, 4]
+]
+
+
+row_oriented_dict_data = [
+    {"c1": 1, "c2": 2, "c3": 4},
+    {"c1": 6, "c2": 3, "c3": 7},
+    {"c1": 7, "c2": 3, "c3": 4}
+]
+
+column_oriented_data = {
+    "c1": [1, 2, 3],
+    "c2": [2, 4, 7],
+    "c3": [4, 2, 1]
+}
+
+# data frame from row_oriented_data
+df1 = pandas.DataFrame(row_oriented_data)
+# print(df1)
+
+# extract a column
+col_series = df1[0]
+# print(col_series)
+
+# extract a row
+row_series = df1.loc[1]
+# print(row_series)
+
+# grab an element
+element = df1.loc[0, 2]
+# print(element)
+
+
+# data fram from row_oriented_dict_data
+df2 = pandas.DataFrame(row_oriented_dict_data)
+# print(df2)
+
+# extract a column
+col_series = df2["c2"]
+# print(col_series)
+
+# extract a row
+row_series = df2.loc[2]
+# print(row_series)
+
+# grab an element
+element = df2.loc[2, "c3"]
+# print(element)
+
+# grab a column with loc (or a range of rows or columns)
+col_series = df2.loc[:, "c3"]
+print(col_series)
---
+++ Python_Scripts/rps.py
@@ -0,0 +1,39 @@
+# Incorporate the random library
+import random
+
+# Specify the three options
+OPTIONS = ["r", "p", "s"]
+
+def is_valid(user_choice):
+    return user_choice in OPTIONS
+
+def show_winner(user_choice, computer_choice):
+    if computer_choice == user_choice:
+        print("Tie")
+    elif (computer_choice == "r" and user_choice == 's') or (computer_choice == "s" and user_choice == 'p') or (computer_choice == "p" and user_choice == 'r'):
+        print("You Lose!")
+    else:
+        print("You Win!")
+
+        
+def get_input():
+    user_choice = input("Make your Choice: (r)ock, (p)aper, (s)cissors? ")
+    
+    if not is_valid(user_choice):
+        raise Exception("Invalid: enter r, p, or s!")
+    
+    return user_choice
+
+
+def main():
+    print("Let's Play Rock Paper Scissors!")
+
+    user_choice = get_input()
+    computer_choice = random.choice(OPTIONS)
+     
+    print(f"You chose {user_choice} and the computer chose {computer_choice}")
+
+    show_winner(user_choice, computer_choice)
+
+
+main() 
